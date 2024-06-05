a4b0e0b1cb18dbecbad90c4e759b57301e61d370
Juan Manuel Diez <jm10yap@gmail.com>
2024-06-04 21:01:31 -0300

    changing app

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,14 +7,14 @@ import Project from "./pages/Project";
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/personal" element={<PersonalInfo />} />
         <Route path="/projects" element={<AllProjects />} />
         <Route path="/project/:id" element={<Project />} />
       </Routes>
-    </div>
+    </section>
   );
 }
 
