61d9f4358fb8d9a271121a1580ef512c7d865665
Juan Manuel Diez <jm10yap@gmail.com>
2025-04-01 21:07:06 +0200

    initial backend with entities

---
+++ Portfolio-backend/src/main/java/com/example/Portfolio/entities/Admin.java
@@ -0,0 +1,64 @@
+package com.example.Portfolio.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "admins")
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public Admin(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ Portfolio-backend/src/main/java/com/example/Portfolio/entities/Project.java
@@ -0,0 +1,99 @@
+package com.example.Portfolio.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.List;
+import java.util.Set;
+
+@Table(name = "projects")
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "technologies")
+    private Set<String> technologies;
+
+    @Column(name = "personal")
+    private boolean personal;
+
+    public Project(String title, String description, String image, Set<String> technologies, boolean personal) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.technologies = technologies;
+        this.personal = personal;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Set<String> getTechnologies() {
+        return this.technologies;
+    }
+
+    public void setTechnologies(Set<String> technologies) {
+        this.technologies = technologies;
+    }
+
+    public void addTechnologies(Set<String> technologies) {
+        this.technologies.addAll(technologies);
+    }
+
+    public void removeTechnologies(Set<String> technologies) {
+        this.technologies.removeAll(technologies);
+    }
+
+    public boolean isPersonal() {
+        return this.personal;
+    }
+
+    public void setPersonal(boolean personal) {
+        this.personal = personal;
+    }
+}
